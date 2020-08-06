7aa425d022ba38e3743b74aee09faa41254470ba
wiebejordan <wiebe.jordan@yahoo.com>
2020-08-06 12:58:43 -0600

    fixed server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const express = require('express'),
 
 app.use(express.json());
 
-app.use(express.static(__dirname + '/..build'))
+app.use(express.static(`${__dirname}/../build`))
 
 app.use(session({
   resave: true,
